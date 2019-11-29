c55852c3dd38753b7deaf9e817bf32a078fa4199
Victor Espejo <contactovictore@gmail.com>
2019-11-29 15:42:00 -0500

    first commit

---
+++ src/View/Main.java
@@ -0,0 +1,30 @@
+package View;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+	PApplet.main(Main.class.getName());
+
+	}
+	
+	public void settings(){
+		size(800,600);
+	}
+	
+	public void setup(){
+		background(0);
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void mouseClicked() {
+		
+	}
+	
+
+}
